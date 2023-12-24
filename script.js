091f11c48549c0a011bfcd2b6ad242293d63966b
SOMESHKUMAR S <154727744+ssomeshkumar@users.noreply.github.com>
2023-12-24 06:28:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+//select popup button,popupoverlay,popupbox
+var addpopupbutton=document.getElementById("add popup button")
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+//eventliser
+addpopupbutton.addEventListener("click",function() {
 
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+
+})
+
+//cancel
+var cancelpopup=document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(){
+    event.preventDefault()
+     popupoverlay.style.display = "none"
+     popupbox.style.display = "none"
+
+})
+
+// container,add-
+
+var container= document.querySelector(".container")
+var addbook= document.getElementById("add-book")
+var booktitleinput= document.getElementById("book-title-input")
+var booksdescriptioninput= document.getElementById("book-description-input")
+var bookauthorinput= document.getElementById("book-author-input")
+
+addbook.addEventListener("click",function(event){
+  event.preventDefault();
+  var div = document.createElement("div");
+  div.setAttribute("class", "book-container");
+  div.innerHTML = `<h2>${booktitleinput.value}</h2>
+        <h5>${bookauthorinput.value}</h5>
+        <p>${booksdescriptioninput.value}</p>
+     <button onclick="deletebook(event)">Delete</button> `
+     popupoverlay.style.display = "none"
+     popupbox.style.display = "none"
+
+
+  container.append(div);
+})
+
+function deletebook(event)
+{
+    event.target.parentElement.remove()
+}
